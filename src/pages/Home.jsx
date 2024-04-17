627e6a2fb31c641f1452240a5c3e614cb280ff76
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2024-04-17 13:43:42 +0530

    updated favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,11 +21,11 @@ const Home = () => {
   const [active, setActive] = useState(heroGrid[0]);
   return (
     <Section>
-      <figure className='relative lg:h-96 h-4/5 w-full px-6 block lg:hidden'>
+      <figure className='relative h-4/5 w-full md:px-6 block lg:hidden'>
         <img
           src='./assets/images/safetyFirst.png'
           alt='safety logo'
-          className='absolute -right-2 -top-3 h-40 w-40 lg:right-60 lg:top-4 lg:h-60 lg:w-60 bg-white rounded-full shadow-xl shadow-blue-gray-900/50'
+          className='absolute -right-4 md:-right-2 -top-3 h-28 w-28 md:h-40 md:w-40 bg-white rounded-full shadow-xl shadow-blue-gray-900/50'
         />
         <img
           className='h-full w-full lg:w-1/2 rounded-xl object-fill object-center shadow-xl shadow-blue-gray-900/50'
@@ -33,7 +33,7 @@ const Home = () => {
           alt={active.alt}
         />
 
-        <figcaption className='absolute bottom-8 left-2/4 flex w-[calc(100%-6rem)] -translate-x-2/4 justify-between rounded-xl border border-white bg-white/75 py-4 px-6 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm'>
+        <figcaption className='absolute bottom-8 left-2/4 flex w-[calc(100%-4rem)] md:w-[calc(100%-6rem)] -translate-x-2/4 justify-between rounded-xl border border-white bg-white/75 py-4 px-6 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm'>
           <Typography
             variant='h5'
             color='blue-gray'
@@ -44,10 +44,14 @@ const Home = () => {
             government sectors. Whether you're looking for installation,
             maintenance, or repair, our dedicated team is here to support you
             every step of the way.
+            <br />
+            <span className='font-extrabold'>Our Values: </span> Safety first,
+            team work, respect, excellence, customer satisfaction BECIS, and DE
+            energy they are the clients
           </Typography>
         </figcaption>
       </figure>
-      <div className='grid lg:hidden grid-cols-3 gap-1 md:gap-4 px-6 pt-4'>
+      <div className='grid lg:hidden grid-cols-3 gap-1 md:gap-4 md:px-6 pt-4'>
         {heroGrid.map((item, index) => (
           <div key={index}>
             <img
@@ -71,7 +75,7 @@ const Home = () => {
         <figure className='relative'>
           <img
             className='h-full w-full rounded-xl object-fill object-center shadow-xl shadow-blue-gray-900/50'
-            src='./assets/images/hero1.jpg'
+            src='./assets/images/hero3.jpg'
             alt='image2'
           />
         </figure>
@@ -92,6 +96,10 @@ const Home = () => {
           government sectors. Whether you're looking for installation,
           maintenance, or repair, our dedicated team is here to support you
           every step of the way.
+          <br />
+          <span className='font-extrabold'>Our Values: </span> Safety first,
+          team work, respect, excellence, customer satisfaction BECIS, and DE
+          energy they are the clients
         </Typography>
       </figcaption>
     </Section>
